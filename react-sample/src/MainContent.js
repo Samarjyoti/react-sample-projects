8a117d08d86b8320f5ed961ab2dce80878e429a3
Samarjyoti Das <samarjyoti.das123@gmail.com>
2024-03-18 00:15:24 +0530

    Minor tweaks

--- react-sample/src/MainContent.js
+++ react-sample/src/MainContent.js
@@ -4,10 +4,14 @@ export default function MainContent() {
     return (
         <div className="maincontent">
             <h1 className="contentHeading">Fun facts about React</h1>
-            <ul>
-                <li className="contentList">Was first released in 2013</li>
-                <li className="contentList">Has over 100K stars on Github</li>
-                <li className="contentList">Is maintained by Facebook</li>
+            <ul className="contentList">
+                <li >Was first released in 2013</li>
+                <li >Has over 100K stars on Github</li>
+                <li >Is maintained by Facebook</li>
+                <li >Was originally created by Jordan Walke</li>
+                <li >Has well over 100K stars on GitHub</li>
+                <li >Is maintained by Facebook</li>
+                <li >Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </div>
     )
